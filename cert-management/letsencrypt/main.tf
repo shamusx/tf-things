c52596e12239fb722ca4ecac95a6bebc3ac1a14e
Shawn Watson <shamusx@gmail.com>
2023-12-07 15:38:37 -0500

    variablize terraform bits

--- cert-management/letsencrypt/main.tf
+++ cert-management/letsencrypt/main.tf
@@ -17,19 +17,19 @@ resource "tls_private_key" "private_key" {
 
 resource "acme_registration" "reg" {
   account_key_pem = tls_private_key.private_key.private_key_pem
-  email_address   = "test.email.int"
+  email_address   = var.user_email
 }
 
 resource "acme_certificate" "certificate" {
   account_key_pem           = acme_registration.reg.account_key_pem
-  common_name               = "tftest.gcp.sandbox.test.int"
-  subject_alternative_names = ["tftest.gcp.sandbox.test.int"]
+  common_name               = var.server_cert_cn
+  subject_alternative_names = [var.server_cert_cn]
   disable_complete_propagation = true
 
   dns_challenge {
     provider = "gcloud"
     config = {
-        GCE_PROJECT = "dns-project"
+        GCE_PROJECT = var.gcp_project_id
     }
   }
 }
